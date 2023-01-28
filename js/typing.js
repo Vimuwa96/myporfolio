23f93fe55c39b3292db54f9856cb342bac7fd2ce
Vimuwa96 <114574555+Vimuwa96@users.noreply.github.com>
2023-01-28 13:25:54 +0530

    Add files via upload

---
+++ js/typing.js
@@ -0,0 +1,16 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Hello!')
+    .pauseFor(2500)
+    .deleteChars(6)
+    .typeString('Hola!')
+    .pauseFor(2500)
+    .deleteChars(7)
+    .typeString('Konnichiwa!')
+    .pauseFor(2500)
+    .deleteChars(11)
+    .start();
